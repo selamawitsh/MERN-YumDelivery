ca909b176e1e718c898c55d23a801d1e8e8680d0
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-03-18 06:47:22 +0300

    adding explore menu and header on home page

---
+++ front-end/src/component/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your Favourite Food Here</h2>
+            
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredienets and culinary expertise.</p>
+            <button>View Menu</button>
+
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Header
