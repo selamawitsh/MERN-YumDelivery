4c76ed71ad92ea45a00e92e6486b8c57eba65c5c
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-03-19 23:03:38 +0300

    adding footer component for the pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,9 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-
+import Footer from './component/Footer/Footer';
 function App() {
   return (
+    <>
     <div className='app'>
       <Navbar />
       <Routes>
@@ -15,6 +16,11 @@ function App() {
         <Route path ='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    
+    <Footer/>
+    </>
+    
+    
   );
 }
 
